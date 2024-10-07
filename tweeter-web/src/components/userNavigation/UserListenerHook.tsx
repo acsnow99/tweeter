26285237a02ccd0308c0c727e9a4e7965ddd354f
Alex Snow <45021795+acsnow99@users.noreply.github.com>
2024-10-07 15:37:51 -0600

    Moved hook code into MVP model

--- tweeter-web/src/components/userNavigation/UserListenerHook.tsx
+++ tweeter-web/src/components/userNavigation/UserListenerHook.tsx
@@ -1,8 +1,7 @@
 import { AuthToken, FakeData, User } from "tweeter-shared";
-import { UserInfoContext } from "../userInfo/UserInfoProvider";
-import { useContext } from "react";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "./UserHook";
+import { UserNavigationPresenter } from "../../presenters/UserNavigationPresenter";
 
 interface UserNavigationListener {
     navigateToUser: (event: React.MouseEvent) => Promise<void>;
@@ -13,42 +12,15 @@ const useUserNavigationListener = (): UserNavigationListener => {
         useUserInfo();
     const { displayErrorMessage } = useToastListener();
 
-    const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-        event.preventDefault();
-    
-        try {
-          const alias = extractAlias(event.target.toString());
-    
-          const user = await getUser(authToken!, alias);
-    
-          if (!!user) {
-            if (currentUser!.equals(user)) {
-              setDisplayedUser(currentUser!);
-            } else {
-              setDisplayedUser(user);
-            }
-          }
-        } catch (error) {
-          displayErrorMessage(`Failed to get user because of exception: ${error}`);
-        }
-      };
-    
-      const extractAlias = (value: string): string => {
-        const index = value.indexOf("@");
-        return value.substring(index);
-      };
-    
-      const getUser = async (
-        authToken: AuthToken,
-        alias: string
-      ): Promise<User | null> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.findUserByAlias(alias);
-      };
-
-    return {
-        navigateToUser
+    const view = {
+      setDisplayedUser: setDisplayedUser,
+      displayErrorMessage: displayErrorMessage,
     }
+    const userPresenter = new UserNavigationPresenter(view);
+
+    const navigateToUser = (event: React.MouseEvent) => userPresenter.navigateToUser(authToken, currentUser, event);
+
+    return { navigateToUser };
 }
 
 export default useUserNavigationListener;
--- tweeter-web/src/model/service/UserService.ts
+++ tweeter-web/src/model/service/UserService.ts
@@ -67,4 +67,13 @@ export class UserService {
     // TODO: Replace with the result of calling server
     return FakeData.instance.getFolloweeCount(user.alias);
   };
+
+      
+  public async getUser (
+    authToken: AuthToken,
+    alias: string
+  ): Promise<User | null> {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.findUserByAlias(alias);
+  };
 }
