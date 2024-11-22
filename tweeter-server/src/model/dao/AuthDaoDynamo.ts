c1c740f0352e657556a72cefdb52249dd6eeebff
Alex Snow <acsnow99@gmail.com>
2024-11-21 19:02:20 -0700

    Register saves authToken to db

--- tweeter-server/src/model/dao/AuthDaoDynamo.ts
+++ tweeter-server/src/model/dao/AuthDaoDynamo.ts
@@ -4,18 +4,22 @@ import { AuthTokenDto } from "tweeter-shared/src";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { genSalt, hash } from "bcryptjs";
+import crypto from 'crypto';
 
 export class AuthDaoDynamo implements AuthDao {
     private readonly client = DynamoDBDocumentClient.from(new DynamoDBClient());
-    private tableName = "tweeter-passwords";
+    private readonly passwordTableName = "tweeter-passwords";
+    private readonly sessionTableName = "tweeter-sessions";
     private readonly aliasAttr = "alias";
     private readonly passwordAttr = "password";
+    private readonly tokenAttr = "token";
+    private readonly timestampAttr = "timestamp";
 
     public async createUserPassword(alias: string, password: string) {
         const salt = await genSalt();
         const hashedPassword = await hash(password, salt);
         const params = {
-            TableName: this.tableName,
+            TableName: this.passwordTableName,
             Item: {
               [this.aliasAttr]: alias,
               [this.passwordAttr]: hashedPassword,
@@ -26,10 +30,20 @@ export class AuthDaoDynamo implements AuthDao {
     }
 
     public async createSession(alias: string, password: string) {
-        return {
-            token: FakeData.instance.authToken.token,
-            timestamp: 0
+        const token = crypto.randomBytes(32).toString('hex');
+        const authToken = {
+            token: token,
+            timestamp: Date.now()
         };
+        const params = {
+            TableName: this.sessionTableName,
+            Item: {
+              [this.tokenAttr]: token,
+              [this.timestampAttr]: authToken.timestamp
+            },
+        };
+        await this.client.send(new PutCommand(params));
+        return authToken;
     }
 
     public deleteSession(authToken: AuthTokenDto) {
