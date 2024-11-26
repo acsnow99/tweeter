0196eeaaa2c1679a8d7831c8484063d67fdb1fdb
Alex Snow <acsnow99@gmail.com>
2024-11-25 19:06:53 -0700

    FollowDAO interface

--- tweeter-server/src/model/dao/FollowDao.ts
+++ tweeter-server/src/model/dao/FollowDao.ts
@@ -1,4 +1,10 @@
+import { UserDto } from "tweeter-shared";
 
 interface FollowDao {
-    
+    readonly insertFollowRelationship: (alias: string, toFollowAlias: string) => void;
+    readonly deleteFollowRelationship: (alias: string, toUnfollowAlias: string) => void;
+    readonly getFollowers: (alias: string) => UserDto[];
+    readonly getFollowees: (alias: string) => UserDto[];
+    readonly getFollowerCount: (alias: string) => number;
+    readonly getFolloweeCount: (alias: string) => string;
 }
